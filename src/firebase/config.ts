8e00be8eb69bc80c994cf076a58381f7a0eeb533
abhay Rathour <abhayrat600@gmail.com>
2025-12-31 07:25:39 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5455681471-6a9b7",
+  "appId": "1:217556320126:web:5eb284890e155496807d2f",
+  "apiKey": "AIzaSyC4BycTKZDDjS2F1IJL2wVRawRtIrzpW4k",
+  "authDomain": "studio-5455681471-6a9b7.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "217556320126"
+};
