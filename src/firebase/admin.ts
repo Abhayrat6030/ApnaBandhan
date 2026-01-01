0ccb5692f706bc201a52ae77265f5e5d18a6e11a
abhay Rathour <abhayrat600@gmail.com>
2026-01-01 11:46:12 +0000

    Try fixing this error: `Runtime Error: The default Firebase app does not

--- src/firebase/admin.ts
+++ src/firebase/admin.ts
@@ -4,16 +4,23 @@ import * as admin from 'firebase-admin';
 
 if (!admin.apps.length) {
   try {
+    const serviceAccount = {
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+    };
+
+    // Check if all required environment variables are present
+    if (!serviceAccount.projectId || !serviceAccount.clientEmail || !serviceAccount.privateKey) {
+        throw new Error('Firebase Admin SDK service account credentials are not set in environment variables.');
+    }
+
     admin.initializeApp({
-      credential: admin.credential.cert({
-        projectId: process.env.FIREBASE_PROJECT_ID,
-        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-        privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-      }),
+      credential: admin.credential.cert(serviceAccount),
       databaseURL: `https://${process.env.FIREBASE_PROJECT_ID}-default-rtdb.firebaseio.com`,
     });
   } catch (error: any) {
-     if (!/already exists/u.test(error.message)) {
+    if (!/already exists/u.test(error.message)) {
       console.error('Firebase admin initialization error', error.stack);
     }
   }
