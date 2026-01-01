c7df7c2d7385bb726c17d2d15dafcf0fcf38b97f
abhay Rathour <abhayrat600@gmail.com>
2026-01-01 05:12:07 +0000

    Try fixing this error: `Console Error: The default Firebase app does not

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,14 +1,10 @@
 
-import { initializeApp, getApps, getApp, FirebaseApp } from 'firebase/app';
-import { getAuth, Auth } from 'firebase/auth';
-import { getFirestore, Firestore } from 'firebase/firestore';
-
 export const firebaseConfig = {
   apiKey: "AIzaSyC4BycTKZDDjS2F1IJL2wVRawRtIrzpW4k",
   authDomain: "studio-5455681471-6a9b7.firebaseapp.com",
   databaseURL: "https://studio-5455681471-6a9b7-default-rtdb.firebaseio.com",
   projectId: "studio-5455681471-6a9b7",
-  storageBucket: "studio-5455681471-6a9b7.firebasestorage.app",
+  storageBucket: "studio-5455681471-6a9b7.appspot.com",
   messagingSenderId: "217556320126",
   appId: "1:217556320126:web:5eb284890e155496807d2f"
 };
