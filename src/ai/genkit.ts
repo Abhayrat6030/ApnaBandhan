8f0f4ac3930128b4659c3f281d0b33aa08ecc66b
abhay Rathour <abhayrat600@gmail.com>
2026-01-03 03:53:34 +0000

    AB EY  proble aa hri ahi

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -2,6 +2,6 @@ import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/google-genai';
 
 export const ai = genkit({
-  plugins: [googleAI({model: 'gemini-pro'})],
-  model: 'googleai/gemini-pro',
+  plugins: [googleAI({model: 'gemini-1.5-flash-latest'})],
+  model: 'googleai/gemini-1.5-flash-latest',
 });
