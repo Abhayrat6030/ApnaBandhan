f20e136fcd6112eff532b90f02775b7cb7de996b
abhay Rathour <abhayrat600@gmail.com>
2026-01-03 03:55:05 +0000

    ye proble aa rhi hai ab

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,7 +1,9 @@
+'use server';
+
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/google-genai';
 
 export const ai = genkit({
-  plugins: [googleAI({model: 'gemini-1.5-flash-latest'})],
-  model: 'googleai/gemini-1.5-flash-latest',
+  plugins: [googleAI()],
+  model: 'googleai/gemini-pro',
 });
